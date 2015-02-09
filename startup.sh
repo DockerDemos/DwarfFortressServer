a21e7ebeec526168ce124856d219bd93736a1d92
Chris Collins <collins.christopher@gmail.com>
2015-02-09 08:10:01 -0500

    symlink vnc.html to index.html so it will be there without a custom URL

--- startup.sh
+++ startup.sh
@@ -3,6 +3,6 @@ export DISPLAY=:1
 Xvfb :1 -screen 0 1280x1024x16 &
 openbox-session&
 x11vnc -display :1 -bg -nopw -listen localhost -xkb -ncache 10 -ncache_cr
-cd /root/noVNC && ./utils/launch.sh --vnc localhost:5900 &
+cd /root/noVNC && ln -s vnc.html index.html && ./utils/launch.sh --vnc localhost:5900 &
 
 exec /df_linux/df 
